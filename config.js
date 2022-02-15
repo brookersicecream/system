d06600ca122d4dd34af2721c75dfa2118bb06306
Gove Allen <gove.allen@gmail.com>
2022-02-15 14:58:04 -0700

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+//This function is used to point the client-side code to the Google App Script server that handles data requests. Every time the code in Google App Script is modified, the server must be deployed again. When that happens the code returned by the function must be updated.
+
+function gas_deployment_id(){return(
+'AKfycbwQdEt0zu-5Lx0pIZFp3rnAKIcsxoTH9Ku3ayJ9tYA9HSFVvhxgus-ICIld-FqM7CCSWw'
+)}
