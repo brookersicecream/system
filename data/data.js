cf7a49e5d6c19a5161edb9b55ba7045bdae9753d
brookersicecream <99557051+brookersicecream@users.noreply.github.com>
2022-02-17 13:28:57 -0700

    Update data.js

--- data/data.js
+++ data/data.js
@@ -1,5 +1,5 @@
 // This is for really stable data so we don't have to ask for it every time. Since stores, rarely change and we aren't worried about this information being available to the user, we store the set of stores and their primary key values from Airtable.
-
+console.log("--------------------------------------------------->3")
 store_sequence=[
     "Vineyard",
     "Herriman",
