c3de12062f4c14657c39c6a6e1939271876ff97b
brookersicecream <99557051+brookersicecream@users.noreply.github.com>
2022-02-17 13:52:41 -0700

    Updating Thu Feb 17 2022 15:52:41 GMT-0500 (Eastern Standard Time)

--- data/data.js
+++ data/data.js
@@ -1,16 +1,1 @@
-// This is for really stable data so we don't have to ask for it every time. Since stores, rarely change and we aren't worried about this information being available to the user, we store the set of stores and their primary key values from Airtable.
-console.log("--------------------------------------------------->3")
-store_sequence=[
-    "Vineyard",
-    "Herriman",
-    "Provo"
-]
-
-stores={
-    recxfZ0QqJQwKKlMC:"Herriman",
-    recPqTEeqvuIVsUHO:"Provo",
-    recoGefTBA6p5ESLF:"Vineyard",
-    Herriman:"recxfZ0QqJQwKKlMC",
-    Provo:"recPqTEeqvuIVsUHO",
-    Vineyard:"recoGefTBA6p5ESLF"
-}
+"\nstore_sequence=[\n    \"Vineyard\",\n    \"Herriman\",\n    \"Provo\"\n]\n\nstores={\n    recxfZ0QqJQwKKlMC:\"Herriman\",\n    recPqTEeqvuIVsUHO:\"Provo\",\n    recoGefTBA6p5ESLF:\"Vineyard\",\n    Herriman:\"recxfZ0QqJQwKKlMC\",\n    Provo:\"recPqTEeqvuIVsUHO\",\n    Vineyard:\"recoGefTBA6p5ESLF\"\n}\n"
