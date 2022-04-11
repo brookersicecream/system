fa416072a939adb56b4827121c10e3b67c5db676
Gove Allen <gove.allen@gmail.com>
2022-04-11 15:17:20 -0600

    fixed login

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 //This function is used to point the client-side code to the Google App Script server that handles data requests. Every time the code in Google App Script is modified, the server must be deployed again. When that happens the code returned by the function must be updated.
 
 function gas_deployment_id(){return(
-  'AKfycbzEeoafkgV_ECTq5_ZaYANXF9OIZogv_7c_HkRGvaYPDmQwlqiqW_Vvaa6TZzNQrggfBw'
+  'AKfycbxmndyFKjtJuXc8_42wEZ5rtLva2qCHsbvwU7OKBQPjt0UijKshM1oCRIxOihMdLTKH-Q'
 )}
 
 
