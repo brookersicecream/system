2a070cb17c1ab2e438c0c94e82fb61a29ca2c7ce
Gove Allen <gove.allen@gmail.com>
2022-08-26 08:56:25 -0600

    newdeployment

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 //This function is used to point the client-side code to the Google App Script server that handles data requests. Every time the code in Google App Script is modified, the server must be deployed again. When that happens the code returned by the function must be updated.
 
 function gas_deployment_id(){return(
-  'AKfycbybyiUZ2UAgytPPkadupvC81oJUlbRxUqkEt1ofdT3xAk3qrIToRWijPU4Jg6zgPr-Ufw'
+  'AKfycbxOo64q-xUAd5Yr2W40KFiODK-5t7GPk6Da4uH_2WcAQ04LfRgtFHdXhTttVwsNTha59g'
 )}
 
 
