cf9238e9aae1ab1d11c94a32dcb64ef44f77ddb0
thegove <gove.allen@gmail.com>
2024-02-24 08:41:04 -0700

    update to PAT

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 //This function is used to point the client-side code to the Google App Script server that handles data requests. Every time the code in Google App Script is modified, the server must be deployed again. When that happens the code returned by the function must be updated.
 
 function gas_deployment_id(){return(
-  'AKfycbwF1ch8dWi5QHd6DjociNhdJypqry29C6rrDDAdVBN7o5KY56Jclwa-BzmEckKhZhhzJg'
+  'AKfycbzJeoxf8Zpn6wyR_znW5PGvr03HiU3I5j-gpyWy8P2JqEm3QzZISz35--CZYLDT3_Dw'
 )}
 
 
